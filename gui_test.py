64491710cfe5a0a207d0bb00ca48cfedce064ddd
Zackzto1 <zackzto@gmail.com>
2026-04-30 20:30:15 -0700

    Add basic GUI for Command Planner application

---
+++ gui_test.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+window = tk.Tk()
+
+window.title("Command Planner")
+window.geometry("400x300")
+
+label = tk.Label(window, text="Command Planner")
+label.pack()
+
+window.mainloop()
